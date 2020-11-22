7924470e2983d0aefa3d8f560cc8bc445f9c0d2d
tiptoemosquito <clhoward33@icloud.com>
2020-11-22 18:16:36 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: %i[show index] do
+    resources :posts, only: %i[show index new edit]
+  end
+
+  resources :posts, only: %i[index show new create edit update]
+
+  namespace :admin do 
+    resources :stats, only: [:index]
+  end 
+
+  root 'posts#index'
 end
